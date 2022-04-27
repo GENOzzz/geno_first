c3daf22d28b9a82fc401c9080c311c2ac6b43b71
GENO_P <92358313+GENOzzz@users.noreply.github.com>
2022-04-27 19:07:50 +0900

    pw일치여부,이름확인

--- src/main/resources/static/js/User/SignUp.js
+++ src/main/resources/static/js/User/SignUp.js
@@ -31,3 +31,41 @@ function id_check(){
         })
 }
 
+function pw_check(){
+    var pw1= $('#pw1').val();
+    var pw2= $('#pw2').val();
+    if(pw1===pw2){
+        $('#pw_success').css("display","inline-block")
+        $('#pw_fail').css("display","none")
+    }else{
+        $('#pw_fail').css("display","inline-block")
+        $('#pw_success').css("display","none")
+    }
+}
+
+function name_check(){
+    var name=$('#name').val();
+    var firstWord=name.slice(0,1);
+    var kor_pattern= /^[가-힣]+$/;
+    var eng_pattern=/^[a-zA-z\s]+$/;
+    console.log(firstWord)
+    if(firstWord!=""){
+        if(kor_pattern.test(firstWord)){
+            if(kor_pattern.test(name)){
+                $('#name_fail').css("display","none")
+            }else{
+                $('#name_fail').css("display","inline-block")
+            }
+        }else if(firstWord!=" "&&eng_pattern.test(firstWord)){
+            if(eng_pattern.test(name)){
+                $('#name_fail').css("display","none")
+            }else{
+                $('#name_fail').css("display","inline-block")
+            }
+        }else{
+            $('#name_fail').css("display","inline-block")
+        }
+    }else{
+        $('#name_fail').css("display","none")
+    }
+}
